09f3c46345e56df1893cd74a6fd5f81eb73be059
Alex <longnamesrule@gmail.com>
2021-03-19 14:00:49 -0400

    message filtering

--- src/main/java/com/alexcomeau/ApiRequest.java
+++ src/main/java/com/alexcomeau/ApiRequest.java
@@ -8,9 +8,11 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ApiRequest {
-    public static String makeRequest() throws IOException {
+    public static String makeRequest(String apiRequest) throws IOException {
         String apiReq= "";
         HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet(apiReq);
@@ -26,5 +28,31 @@ public class ApiRequest {
         }
         return apiResponse.toString();
 
+    }
+
+    public static String formatRequest(String city){
+        if(getSpecialCharacters(city)){
+            return "invalid";
+        }
+
+        return null;
+    }
+
+    public static boolean getSpecialCharacters(String s) {
+        if (s == null || s.trim().isEmpty()) {
+            System.out.println("Incorrect format of string");
+            return true;
+        }
+        Pattern p = Pattern.compile("[^A-Za-z0-9. - ]");
+        Matcher m = p.matcher(s);
+        // boolean b = m.matches();
+        boolean b = m.find();
+        if (b){
+            System.out.println("There is a special character in my string ");
+            return true;
+        } else {
+            System.out.println("There is no special char.");
+            return false;
+        }
     }
 }
--- src/main/java/com/alexcomeau/WeatherCommands.java
+++ src/main/java/com/alexcomeau/WeatherCommands.java
@@ -5,6 +5,13 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class WeatherCommands {
     public static void weatherCommands(MessageReceivedEvent event, Message msg, String prefix){
+        if(msg.getContentRaw().startsWith(prefix + "w ")){
 
+            String[] subs = msg.getContentRaw().split(" ");
+            String city = subs[1];
+
+
+
+        }
     }
 }
