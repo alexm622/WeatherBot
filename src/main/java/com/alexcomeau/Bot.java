892a2c8a7f7619fbbf7f4a4a466123db752b5652
Alex <longnamesrule@gmail.com>
2021-03-19 22:37:25 -0400

    added debug symbols

--- src/main/java/com/alexcomeau/Bot.java
+++ src/main/java/com/alexcomeau/Bot.java
@@ -21,10 +21,12 @@ public class Bot extends ListenerAdapter {
 
         //test for easter eggs
         if(EasterEgg.easterEgg(msg, event, prefix)){
+            Debug.debug("breaking from easter egg");
             return;
         }
         //weather commands test
         try {
+            Debug.debug("trying weather commands");
             WeatherCommands.weatherCommands(event, msg, prefix);
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/alexcomeau/WeatherCommands.java
+++ src/main/java/com/alexcomeau/WeatherCommands.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 public class WeatherCommands {
     public static boolean weatherCommands(MessageReceivedEvent event, Message msg, String prefix) throws IOException {
+        Debug.debug("does message start with &w: " + msg.getContentRaw().startsWith(prefix + "w "));
         if(msg.getContentRaw().startsWith(prefix + "w ")){
 
             String[] subs = msg.getContentRaw().split(" ");
@@ -23,11 +24,15 @@ public class WeatherCommands {
             }
 
             String city = sb.toString().trim();
+            Debug.debug("got city of " + city);
 
             city=ApiRequest.cleanRequest(city);
+            Debug.debug("cleaned " + city);
 
             String request = ApiRequest.createRequest(city);
+            Debug.debug("got request request of " + request);
             String output = ApiRequest.makeRequest(request);
+            Debug.debug("got request output of " + request);
             MessageChannel channel = event.getChannel();
             channel.sendMessage(output) /* => RestAction<Message> */
                     .queue();
